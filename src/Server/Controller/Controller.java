d7eb6690ab96bd97555c4a80791709214dfe69bd
tranhuuchinh0902 <tranhuuchinh500@gmail.com>
2022-12-01 01:48:59 +0700

    Controller Server Frame

--- src/Server/Controller/Controller.java
+++ src/Server/Controller/Controller.java
@@ -1,5 +1,145 @@
 package Server.Controller;
 
-public class Controller {
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import Client.ClientApp;
+import Server.Views.Main;
 
+public class Controller extends Thread {
+	ClientApp thisClient;
+
+	public Controller(Socket clientSocket) {
+		try {
+			thisClient = new ClientApp();
+//			thisClient.socket = clientSocket;
+			OutputStream os = clientSocket.getOutputStream();
+//			thisClient.sender = new BufferedWriter(new OutputStreamWriter(os, StandardCharsets.UTF_8));
+			InputStream is = clientSocket.getInputStream();
+//			thisClient.receiver = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+//			thisClient.port = clientSocket.getPort();
+		} catch (IOException e) {
+
+		}
+	}
+	@Override
+	public void run() {
+		try {
+			while (true) {
+				String header = thisClient.receiver.readLine();
+				if (header == null)
+					throw new IOException();
+
+				System.out.println("Header: " + header);
+				switch (header) {
+					case "filterList": {
+						break;
+					}
+					case "addAccount": {
+						break;
+					}
+					case "updateAccount": {
+						break;
+					}
+					case "removeAccount": {
+						break;
+					}
+					case "lockAccount": {
+						break;
+					}
+					case "historyLogin": {
+						break;
+					}
+					case "listFriend": {
+						break;
+					}
+					case "listLoginTime": {
+						break;
+					}
+					case "listGroupChat": {
+						break;
+					}
+					case "signUp": {
+						break;
+					}
+					case "logIn": {
+						break;
+					}
+					case "forgotPassword": {
+						break;
+					}
+					case "addFriend": {
+						break;
+					}
+					case "unFriend": {
+						break;
+					}
+					case "listFriendOnline": {
+						break;
+					}
+					case "chatWithFriendOnline": {
+						break;
+					}
+					case "chatWithFriendOffline": {
+						break;
+					}
+					case "viewChatHistory": {
+						break;
+					}
+					case "romoveChatHistory": {
+						break;
+					}
+					case "searchStringChatSingle": {
+						break;
+					}
+					case "searchStringChatMultiple": {
+						break;
+					}
+					case "createGroup": {
+						break;
+					}
+					case "changeNameGroup": {
+						break;
+					}
+					case "administator": {
+						break;
+					}
+					case "removeMember": {
+						break;
+					}
+					case "chatGroup": {
+						break;
+					}
+				}
+			}
+
+		} catch (IOException e) {
+			if (!Main.socketController.s.isClosed() /*&& thisClient.userName != null*/) {
+
+				try {
+//					for (ClientApp client : Main.socketController.connectedClient) {
+//						if (!client.userName.equals(thisClient.userName)) {
+//							client.sender.write("user quit");
+//							client.sender.newLine();
+//							client.sender.write(thisClient.userName);
+//							client.sender.newLine();
+//							client.sender.flush();
+//						}
+//					}
+
+//					for (Room room : Main.socketController.allRooms)
+//						room.users.remove(thisClient.userName);
+
+//					thisClient.socket.close();
+
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				Main.socketController.connectedClient.remove(thisClient);
+//				Main.mainScreen.updateClientTable();
+			}
+		}
+	}
 }
---
+++ src/Server/Views/Main.java
@@ -0,0 +1,13 @@
+package Server.Views;
+
+import Server.Controller.SocketController;
+
+public class Main {
+	public static SocketController socketController;
+	public static MainScreen mainScreen;
+
+	public static void main(String[] args) {
+		mainScreen = new MainScreen();
+
+	}
+}
